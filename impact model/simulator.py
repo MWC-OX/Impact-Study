731cf6f70043bfaf96cb45530702a580e218a8df
m-Chan24 <140012163+m-Chan24@users.noreply.github.com>
2025-02-22 13:53:48 +0000

    implemented simulator

--- impact model/simulator.py
+++ impact model/simulator.py
@@ -4,17 +4,41 @@ from person import Person
 class Simulator:
 
     def __init__(self):
-        self.people = []
-
-    def add_people(self, number_of_people, lower, upper):
-        for i in range(number_of_people):
-            self.people.append(Person.generate_person(lower, upper))
-
-    def age_people(self, dt):
-        for person in self.people:
-            person.tic(dt)
-
-    def run(self, years, dt):
-        for i in range( round(years/dt) ):
-            self.age_people(1)
+        self.data = []
         
+
+    def simulate_person(self, upper=80, lower=20, dt=1):
+
+        subject = Person.generate_person(upper, lower)
+        result = {}
+
+        result["race"] = subject.get_race()
+
+        while subject.get_state() != "DEAD":
+
+            result["age"].append(subject.get_age())
+            result["state"].append(subject.get_state())
+            result["risk score"].append(subject.get_risk_score())
+            result["bmi"].append(subject.get_bmi())
+
+            subject.tic(dt)
+        
+        result["costs"] = subject.b_cancer_costs
+
+        self.data.append(result)
+
+
+    def simulate_population(self, n=1000, upper=80, lower=20, dt=1):
+
+        self.clear_data()
+
+        for _ in range(n):
+            self.simulate_person(upper, lower, dt)
+
+        return self.data
+    
+    def clear_data(self):
+        self.data = []
+
+    
+
